9b9ab026a426c75f145ade3a042c76662e75e5ce
Kayode <kayode@MacBook-Pro.local>
2015-10-23 16:32:31 -0400

    Refactored for SauceLabs

--- src/test/java/com/everwise/tests/EverwiseTest.java
+++ src/test/java/com/everwise/tests/EverwiseTest.java
@@ -6,21 +6,38 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import com.everwise.pages.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
 
 
-public class EverwiseTests {
+
+public class EverwiseTest {
 	private WebDriver driver;
 	private String username = "test-student@geteverwise.com";
 	private String password = "ewadmin123";
+
+	public static final String USERNAME = "kayode";
+  	public static final String ACCESS_KEY = "49582d86-2047-44fe-9f8a-48ba7af573fd";
+  	public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
 	//private HomePage hp;
 	//private Missions missionpage;
 
 	@BeforeMethod
-	public void initialize(){
-		driver = new FirefoxDriver();
+	public void initialize() throws Exception {
+
+
+		//driver = new FirefoxDriver();
+		
+		 DesiredCapabilities caps = DesiredCapabilities.chrome();
+    	caps.setCapability("platform", "Windows XP");
+    	caps.setCapability("version", "43.0");
+
+    	WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://o26.ewhosts.net");
 		}
+		
 	@AfterMethod
 	public void cleanUp(){
 		driver.quit();
@@ -44,7 +61,7 @@ public class EverwiseTests {
 		missionpage.missionSort(3);
 		//System.out.println(driver.getCurrentUrl());
 		String title = missionpage.missionTitle();
-		Assert.assertEquals(title,"You’ve had your first meeting, now what?");
+		Assert.assertEquals(title,"Youï¿½ve had your first meeting, now what?");
 	}
 
 	
